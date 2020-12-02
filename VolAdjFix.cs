54ee80084326616bf9273eba054682201270c13e
Sarim Khan <sarim2005@gmail.com>
2020-12-02 06:51:21 +0600

    Only set volume when a volumn change notification is received

--- VolAdjFix.cs
+++ VolAdjFix.cs
@@ -1,21 +1,40 @@
 using System;
 using VideoPlayerController;
 
+public class VolumeListener
+{
+    private float TargetVolume;
+
+    public VolumeListener(float targetVolume) {
+        TargetVolume = targetVolume;
+    }
+
+    public void OnVolumeNotification(AudioVolumeNotificationData notificationData)
+    {
+        float Volume = notificationData.MasterVolume * 100;
+        if (Math.Abs(Volume - TargetVolume) >= 1) {
+            AudioManager.SetMasterVolume(TargetVolume);
+            Console.WriteLine("Volume Adjusted to: {0} from: {1}", TargetVolume, Volume);
+        }
+    }
+}
+
 public static class Gittu
 {
     static void Main(string[] args)
     {
-        float volumeLevel;
         float targetVolume = float.Parse(args[0]);
+        VolumeListener listener;
+
         Console.WriteLine("VolAdj Monitoring started. Target Volume: {0}", targetVolume);
+        AudioManager.SetMasterVolume(targetVolume);
+
+        listener = new VolumeListener(targetVolume);
+        AudioManager.SetMasterVolumeNotifier(listener);
+
         while(true)
         {
-            volumeLevel = AudioManager.GetMasterVolume();
-            if (Math.Abs(volumeLevel - targetVolume) >= 1) {
-                AudioManager.SetMasterVolume(targetVolume);
-                Console.WriteLine("Volume Adjusted to: {0} from: {1}", targetVolume, volumeLevel);
-                System.Threading.Thread.Sleep(50);
-            }
+            System.Threading.Thread.Sleep(10000);
         }
     }
 }
